a39ce99754cb7cdcd8a22f685f89083c07c42ac8
Mohan <dmohan9848@gmail.com>
2025-06-23 20:46:09 +0530

    Updated package.json and fixed Cloudinary config

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,16 +1,18 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-ClooudinaryConfig=cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET
-})
+
+const CloudinaryConfig = cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+});
+
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'wanderlust_dev',
-    allowedFormats:["png","jpg","jpeg"],
+    allowedFormats: ["png", "jpg", "jpeg"],
   },
 });
 
-module.exports={storage,ClooudinaryConfig};
+module.exports = { storage, CloudinaryConfig };
